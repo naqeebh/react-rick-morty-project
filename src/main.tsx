de0381320995698b018b814d9afde992323a8fd8
NaqeebH <Naqeeb.1011@gmail.com>
2025-12-18 18:50:02 +0100

    chore: migrate project to Typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.js'
 
 
 createRoot(document.getElementById('root')).render(
@@ -10,3 +9,5 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+
